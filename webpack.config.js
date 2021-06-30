8a11f9a683d11d44f12b8ad507aaa51989cfed51
ngani faith <nganifaith@gmail.com>
2021-06-30 22:53:56 +0100

    Add project files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,16 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
 module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
   module: {
     rules: [
       {
@@ -8,6 +20,16 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
