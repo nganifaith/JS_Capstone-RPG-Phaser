0e0215c289a51fcd37a16a0a94d17f71b05b376b
ngani faith <nganifaith@gmail.com>
2021-07-01 21:37:45 +0100

    Add title scene

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,11 +1,50 @@
 import Phaser from 'phaser';
+import config from '../Config/config';
 
 export default class TitleScene extends Phaser.Scene {
   constructor() {
     super('Title');
   }
 
-  static preload() {}
+  create() {
+    // Game
+    this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.gameButton, 1);
 
-  static create() {}
+    this.gameText = this.add.text(0, 0, 'Play', {
+      fontSize: '32px',
+      fill: '#fff',
+    });
+    this.centerButtonText(this.gameText, this.gameButton);
+
+    this.gameButton
+      .on('pointerdown', () => {
+        this.scene.start('Game');
+      })
+      .bind(this);
+
+    this.input.on('pointerover', (event, gameObjects) => {
+      gameObjects[0].setTexture('blueButton2');
+    });
+
+    this.input.on('pointerout', (event, gameObjects) => {
+      gameObjects[0].setTexture('blueButton1');
+    });
+  }
+
+  centerButton(gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(
+        config.width / 2,
+        config.height / 2 - offset * 100,
+        config.width,
+        config.height,
+      ),
+    );
+  }
+
+  static centerButtonText(gameText, gameButton) {
+    Phaser.Display.Align.In.Center(gameText, gameButton);
+  }
 }
