c85cb5e3ec0745b2fba89ce0b75ffd21e3168ab3
ngani faith <nganifaith@gmail.com>
2021-07-05 23:02:53 +0100

    Add update function

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,4 @@
 import Phaser from 'phaser';
-import Model from '../Model';
 import config from '../Config/config';
 
 export default class GameScene extends Phaser.Scene {
@@ -27,17 +26,19 @@ export default class GameScene extends Phaser.Scene {
     } else {
       platform = this.physics.add.sprite(posX, config.height * 0.8, 'platform');
       platform.setImmovable(true);
-      platform.setVelocityX(Model.platformStartSpeed * -1);
+      platform.setVelocityX(this.model.platformStartSpeed * -1);
       this.platformGroup.add(platform);
     }
     platform.displayWidth = platformWidth;
     this.nextPlatformDistance = Phaser.Math.Between(
-      Model.spawnRange[0],
-      Model.spawnRange[1],
+      this.model.spawnRange[0],
+      this.model.spawnRange[1]
     );
   }
 
   create() {
+    this.model = this.sys.game.globals.model;
+
     this.platformGroup = this.add.group({
       // once a platform is removed, it's added to the pool
       removeCallback(platform) {
@@ -59,11 +60,11 @@ export default class GameScene extends Phaser.Scene {
 
     // adding the player;
     this.player = this.physics.add.sprite(
-      Model.playerStartPosition,
+      this.model.playerStartPosition,
       config.height / 2,
-      'player',
+      'player'
     );
-    this.player.setGravityY(Model.playerGravity);
+    this.player.setGravityY(this.model.playerGravity);
 
     // setting collisions between the player and the platform group
     this.physics.add.collider(this.player, this.platformGroup);
@@ -74,14 +75,43 @@ export default class GameScene extends Phaser.Scene {
 
   jump() {
     if (
-      this.player.body.touching.down
-      || (this.playerJumps > 0 && this.playerJumps < Model.jumps)
+      this.player.body.touching.down ||
+      (this.playerJumps > 0 && this.playerJumps < this.model.jumps)
     ) {
       if (this.player.body.touching.down) {
         this.playerJumps = 0;
       }
-      this.player.setVelocityY(Model.jumpForce * -1);
+      this.player.setVelocityY(this.model.jumpForce * -1);
       this.playerJumps += 1;
+    }
+  }
+
+  update() {
+    // game over
+    if (this.player.y > config.height) {
+      this.scene.start('PlayGame');
+    }
+    this.player.x = this.model.playerStartPosition;
+
+    // recycling platforms
+    let minDistance = config.width;
+    this.platformGroup.getChildren().forEach((platform) => {
+      const platformDistance =
+        config.width - platform.x - platform.displayWidth / 2;
+      minDistance = Math.min(minDistance, platformDistance);
+      if (platform.x < -platform.displayWidth / 2) {
+        this.platformGroup.killAndHide(platform);
+        this.platformGroup.remove(platform);
+      }
+    }, this);
+
+    // adding new platforms
+    if (minDistance > this.nextPlatformDistance) {
+      const nextPlatformWidth = Phaser.Math.Between(
+        this.model.platformSizeRange[0],
+        this.model.platformSizeRange[1]
+      );
+      this.addPlatform(nextPlatformWidth, config.width + nextPlatformWidth / 2);
     }
   }
 }
