daa7f1ccb57e7c013bcc79a6a86032077c0f0bc5
ngani faith <nganifaith@gmail.com>
2021-07-05 22:37:54 +0100

    Add game options for platform stage

--- src/Model.js
+++ src/Model.js
@@ -2,8 +2,15 @@
 export default class Model {
   constructor() {
     this._soundOn = true;
-    this._musicOn = true;
+    this._musicOn = false;
     this._bgMusicPlaying = false;
+    this._platformStartSpeed = 350;
+    this._spawnRange = [100, 350];
+    this._platformSizeRange = [50, 250];
+    this._playerGravity = 900;
+    this._jumpForce = 400;
+    this._playerStartPosition = 200;
+    this._jumps = 2;
   }
 
   set musicOn(value) {
@@ -28,5 +35,33 @@ export default class Model {
 
   get bgMusicPlaying() {
     return this._bgMusicPlaying;
+  }
+
+  get platformSizeRange() {
+    return this._platformSizeRange;
+  }
+
+  get platformStartSpeed() {
+    return this._platformStartSpeed;
+  }
+
+  get spawnRange() {
+    return this._spawnRange;
+  }
+
+  get playerGravity() {
+    return this._playerGravity;
+  }
+
+  get jumpForce() {
+    return this._jumpForce;
+  }
+
+  get jumps() {
+    return this._jumps;
+  }
+
+  get playerStartPosition() {
+    return this._playerStartPosition;
   }
 }
